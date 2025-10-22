151552210034430aaadaf524495e909bc304b28d
Riyam <cs50riyam211@gmail.com>
2025-10-22 22:02:20 +0300

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo 2',
       theme: ThemeData(primarySwatch: Colors.deepOrange),
       home: const Scaffold(body: Center(child: Text('Automation App'))),
     );
